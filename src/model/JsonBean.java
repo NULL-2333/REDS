f27c07d6fe63653a961b3dc52796466fcb1c9e42
NULL-2333 <450810699@qq.com>
2018-11-15 13:01:28 +0800

    cym

---
+++ src/model/JsonBean.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+//
+public class JsonBean{
+	public String file;
+	public String description;
+	public List<Data> data;
+	
+	public JsonBean(){
+		file = "";
+		description = "";
+		data = new ArrayList<Data>();
+	}
+	public static class Data{
+		public String id;
+		public String text;
+		public String relation;
+		public String comment;
+		public Label label;
+	}
+	
+	public static class Label{
+		public String relation_type;
+	}
+	
+}
